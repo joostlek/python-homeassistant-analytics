c50a4ebe7445300194b47b13ac800fa641de2136
Joost Lekkerkerker <joostlek@outlook.com>
2024-01-19 11:27:05 +0100

    Add integration type (#21)

--- src/python_homeassistant_analytics/models.py
+++ src/python_homeassistant_analytics/models.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
+from enum import StrEnum
 
 from mashumaro import field_options
 from mashumaro.mixins.orjson import DataClassORJSONMixin
@@ -21,8 +22,24 @@ class Analytics(DataClassORJSONMixin):
     current: CurrentAnalytics = field(metadata=field_options(alias="current"))
 
 
+class IntegrationType(StrEnum):
+    """Integration type."""
+
+    INTEGRATION = "integration"
+    VIRTUAL = "virtual"
+    SERVICE = "service"
+    ENTITY = "entity"
+    HUB = "hub"
+    DEVICE = "device"
+    SYSTEM = "system"
+    HELPER = "helper"
+    BRAND = "brand"
+    HARDWARE = "hardware"
+
+
 @dataclass
 class Integration(DataClassORJSONMixin):
     """Integration model."""
 
     title: str
+    integration_type: IntegrationType
