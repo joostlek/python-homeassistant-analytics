bb76398c7a9c0a77b9846d4cd8d3a0f03c5a73ec
Joostlek <joostlek@outlook.com>
2024-09-23 19:17:33 +0200

    Fix

--- tests/const.py
+++ tests/const.py
@@ -2,8 +2,8 @@
 
 from importlib import metadata
 
-HOMEASSISTANT_ANALYTICS_URL = "https://analytics.home-assistant.io:443"
-HOMEASSISTANT_URL = "https://home-assistant.io:443"
+HOMEASSISTANT_ANALYTICS_URL = "https://analytics.home-assistant.io"
+HOMEASSISTANT_URL = "https://home-assistant.io"
 
 version = metadata.version("python_homeassistant_analytics")
 
